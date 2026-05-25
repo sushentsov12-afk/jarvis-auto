e4b64331638c2f17a1052b51265f4c975404edb7
sushentsov12-afk <sushentsov12@gmail.com>
2026-05-25 18:00:07 +0300

    feat: add components index for easy imports

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as DiagnosisComponent } from './DiagnosisComponent.jsx';
+export { default as UrgencyIndicator } from './UrgencyIndicator.jsx';
+export { default as AdvicePanel } from './AdvicePanel.jsx';
+export { default as CarManager } from './CarManager.jsx';
+export { default as DiagnosisHistory } from './DiagnosisHistory.jsx';
+export { default as ServiceLocator } from './ServiceLocator.jsx';
